709e040ca46e6b49ff2cd87e194e5b679301ea76
slade <paran87@mail.ru>
2023-02-25 16:28:05 +0300

    Fist site

---
+++ blog/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello!"
+
+@app.route("/greet/<name>/")
+def greet_name(name: str):
+    return f"Hello {name}!"
